74464c5b9913afa462daab5b385f2f1719172ca4
hasibprangon <hasibulhossainp09@gmail.com>
2023-07-15 20:12:31 +0600

    store multiple data

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,14 +1,37 @@
 // use local storage to manage cart data
 const addToDB = id =>{
-    const quantity = localStorage.getItem(id);
+    let shoppingCart = {};
+
+    //get the shopping cart
+     const storedCart = localStorage.getItem('shopping-acrt');
+     if(storedCart){
+        // console.log(typeof storedCart);
+
+        shoppingCart = JSON.parse(storedCart);
+     }
+     else{
+        shoppingCart ={};
+     }
+
+    //add quantity
+    const quantity = shoppingCart[id];
     if(quantity){
-        console.log('already exist');
+        // console.log('already exist');
+
         const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity);
+        shoppingCart[id] = newQuantity;
+
+        // localStorage.setItem(id, newQuantity);
     }
     else{
-        console.log('New Item');
-        localStorage.setItem(id, 1);
+
+        shoppingCart[id] = 1;
+
+
+        // console.log('New Item');
+        // localStorage.setItem(id, 1);
     }
+
+    localStorage.setItem('shopping-acrt', JSON.stringify(shoppingCart));
 };
 export{addToDB}; 
